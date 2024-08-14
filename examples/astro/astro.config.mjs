f73b9d5ff87f7a34a688bc68d925158f7256e4c1
Saddam Azad <saddam.azad@me.com>
2024-08-14 10:30:03 +0600

    astro 3

--- examples/astro/astro.config.mjs
+++ examples/astro/astro.config.mjs
@@ -7,4 +7,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	vite: {
+		build: {
+			exclude: ['stack.ts']
+		}
+	}
 });
