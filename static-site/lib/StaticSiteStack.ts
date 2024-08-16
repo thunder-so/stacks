f9fb434e1db652b8389dc53df1a0e0c15bd8d883
Saddam Azad <saddam.azad@me.com>
2024-08-16 16:26:55 +0600

    oac

--- static-site/lib/StaticSiteStack.ts
+++ static-site/lib/StaticSiteStack.ts
@@ -1,8 +1,10 @@
-import { Stack } from 'aws-cdk-lib';
+import { Aws, ArnFormat, Stack } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { ApplicationConstruct, EnvironmentConstruct, HostingConstruct, PipelineConstruct } from '../../constructs';
 import type { StaticSiteProps } from './StaticSiteProps'; 
 import { BuildEnvironmentVariableType } from 'aws-cdk-lib/aws-codebuild';
+import { CfnDistribution, CfnOriginAccessControl } from "aws-cdk-lib/aws-cloudfront";
+import { CfnBucketPolicy } from "aws-cdk-lib/aws-s3";
 
 export class StaticSiteStack extends Stack {
   constructor(scope: Construct, id: string, props?: StaticSiteProps) {
@@ -58,6 +60,50 @@ export class StaticSiteStack extends Stack {
       // buildEnvFilePath: props.buildEnvFilePath as string,
       buildEnvironmentVariables: props.buildEnvironmentVariables as Record<string, { value: string; type: BuildEnvironmentVariableType.PARAMETER_STORE }>
     });
+
+    /**
+     * Origin Access Control (OAC) patch
+     * Adapted from: https://github.com/awslabs/cloudfront-hosting-toolkit
+     * 
+     * Patch is needed because no native support from AWS.
+     * https://github.com/aws/aws-cdk/issues/21771
+     */
+    const oac = new CfnOriginAccessControl(this, "OAC", {
+      originAccessControlConfig: {
+        name: `${Aws.STACK_NAME}-oac`,
+        originAccessControlOriginType: "s3",
+        signingBehavior: "always",
+        signingProtocol: "sigv4",
+      },
+    });
+
+    const cfnDistribution = hosting?.distribution.node.defaultChild as CfnDistribution;
+    cfnDistribution.addOverride(
+      "Properties.DistributionConfig.Origins.0.S3OriginConfig.OriginAccessIdentity",
+      ""
+    );
+    cfnDistribution.addPropertyOverride(
+      "DistributionConfig.Origins.0.OriginAccessControlId",
+      oac.getAtt("Id")
+    );
+
+    const comS3PolicyOverride = hosting?.hostingBucket.node.findChild("Policy").node.defaultChild as CfnBucketPolicy;
+    const statement = comS3PolicyOverride.policyDocument.statements[1];
+    if (statement["_principal"] && statement["_principal"].CanonicalUser) {
+      delete statement["_principal"].CanonicalUser;
+    }
+
+    const s3OriginNode = hosting?.distribution.node
+      .findAll()
+      .filter((child) => child.node.id === "S3Origin");
+
+    if (s3OriginNode && s3OriginNode.length > 0) {
+      const resourceNode = s3OriginNode[0].node.findChild("Resource");
+      if (resourceNode) {
+        resourceNode.node.tryRemoveChild("Resource")
+      }
+    }
+    // End of OAC patch
 
   }
 }
