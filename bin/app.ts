c9ff3743cf95222b4dd6600230083c034fa0250c
Saddam Azad <saddam.azad@me.com>
2024-08-18 12:31:20 +0600

    context

---
+++ bin/app.ts
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+import { App } from "aws-cdk-lib";
+import { SPAStack, type SPAProps } from '../';
+
+const app = new App();
+
+const metadata: SPAProps = app.node.tryGetContext('metadata');
+
+if (!metadata) {
+  throw new Error('Context metadata missing!');
+}
+
+const {
+  env,
+  application,
+  service,
+  environment,
+  sourceProps,
+  githubAccessTokenArn,
+  buildProps,
+  buildEnvironmentVariables,
+  domain,
+  globalCertificateArn,
+  hostedZoneId
+} = metadata;
+
+const appStackProps: SPAProps = {
+  env: {
+    account: env.account,
+    region: env.region
+  },
+  application,
+  service,
+  environment,
+  sourceProps: {
+    owner: sourceProps.owner,
+    repo: sourceProps.repo,
+    branchOrRef: sourceProps.branchOrRef,
+    rootdir: sourceProps.rootdir
+  },
+  githubAccessTokenArn,
+  buildProps: {
+    runtime: buildProps?.runtime as number,
+    installcmd: buildProps?.installcmd as string,
+    buildcmd: buildProps?.buildcmd as string,
+    outputdir: buildProps?.outputdir as string
+  },
+  buildEnvironmentVariables,
+  domain,
+  globalCertificateArn,
+  hostedZoneId
+};
+
+new SPAStack(new App(), `${appStackProps.application}-${appStackProps.service}-${appStackProps.environment}-stack`, appStackProps);
+
+app.synth();
